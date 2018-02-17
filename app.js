cded5fc5d7500dc5b0a3d1e4876b8123696079ed
MACEL94 <francesco.belacca.lavoro@gmail.com>
2018-02-18 00:01:09 +0100

    Introdotta integrazione con Mongoose e quindi salvataggio persistente dei dati tramite MongoDB in locale.

--- app.js
+++ app.js
@@ -1,47 +1,97 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var campgrounds = [
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    // Per collegarsi al db e debuggare da powershell: 
+    // ii "C:\Program Files\MongoDB\Server\3.6\bin\mongo.exe"
+    mongoose = require("mongoose");
+
+// Crea o si collega al DB
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+// Schema setup
+var campGroundSchema = new mongoose.Schema(
     {
-        name: "Salmon Creek",
-        image: "http://www.photosforclass.com/download/8288665755"
-    },
-    {
-        name: "Ivana De Bonis's",
-        image: "http://www.photosforclass.com/download/3730341267"
-    },
-    {
-        name: "John Locke's",
-        image: "http://www.photosforclass.com/download/22116667926"
+        name: String,
+        image: String,
+        description: String,
     }
-];
+);
+
+// Schema compile in modelper avere la classe utilizzabile
+var Campground = mongoose.model("Campground", campGroundSchema);
+
+// // Codice esempio che crea un nuovo campGround
+// Campground.create({
+//     name: "Salmon Creek",
+//     image: "http://www.photosforclass.com/download/8288665755",
+//     description: "Bellissima descrizione inutile del mio primo campground."
+// }, function (err, nuovoOggetto) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Nuovo Oggetto Creato!!");
+//         console.log(nuovoOggetto);
+//     }
+// });
 
 // Setto ejs come VE
 app.set("view engine", "ejs");
+
 // Faccio usare il bodyParser a express
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Inizio dell'Applicazione
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-
-    res.render("campgrounds", { campgrounds: campgrounds });
-})
+    Campground.find({}, function (err, campgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { campgrounds: campgrounds });
+        }
+    })
+});
 
 app.post("/campgrounds", function (req, res) {
     // Prendo i dati dal form
     var name = req.body.name;
     var image = req.body.image;
-
-    campgrounds.push({name: name, image: image});
-    // ridirigo alla pagina corretta
-    res.redirect("/campgrounds");
+    var description = req.body.description;
+    var newDBObj = { name: name, image: image, description: description };
+    
+    //Crea il nuovo campground e lo salva in db
+    Campground.create(newDBObj, function (err, campgrounds) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // ridirigo alla pagina corretta
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
+// Nuovo campground
 app.get("/campgrounds/new", function (req, res) {
-    res.render("new.ejs");
+    res.render("new");
+});
+
+// Pagina del singolo CampGround
+app.get("/campgrounds/:id", function (req, res) {
+    // prende il camp tramite l'id passato nell'url
+    Campground.findById(req.params.id, function (err, foundCamp) {
+        if (err) {
+            console.log(err);
+        } else {
+            // lo rendedizza
+            res.render("show", { campground: foundCamp });
+        }
+    })
+
 });
 
 app.listen("3000", function () {
