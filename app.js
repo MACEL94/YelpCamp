2e9f723500fff5f791b7e2240dee0fc3caa76b57
MACEL94 <francesco.belacca.lavoro@gmail.com>
2018-02-18 17:34:09 +0100

    Introdotta la creazione di commenti, anche se ancora manca un pulsante che permetta di andarci senza sapere come funzioano i route. Spostate le view nei folder corretti.

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get("/campgrounds", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("index", { campgrounds: campgrounds });
+            res.render("campgrounds/index", { campgrounds: campgrounds });
         }
     })
 });
@@ -57,7 +57,7 @@ app.post("/campgrounds", function (req, res) {
 
 // Nuovo campground
 app.get("/campgrounds/new", function (req, res) {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 // Pagina del singolo CampGround
@@ -68,7 +68,51 @@ app.get("/campgrounds/:id", function (req, res) {
             console.log(err);
         } else {
             // lo rendedizza
-            res.render("show", { campground: foundCamp });
+            res.render("campgrounds/show", { campground: foundCamp });
+        }
+    })
+});
+
+// COMMENTI
+// Form nuovo commento
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+    //Trova l'oggetto dall'id
+    var campGround = Campground.findById(req.params.id, function (err, dbCamp) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("comments/new", { campground: dbCamp });
+        }
+    });
+});
+
+// Creazione del commento
+app.post("/campgrounds/:id/comments", function (req, res) {
+    // Ho già raggruppato i dati necessari in un oggetto comment
+    //Trova l'oggetto dall'id
+    Campground.findById(req.params.id, function (err, dbCamp) {
+        //Crea il nuovo commento in db
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            // Creo il nuovo commento
+            Comment.create(req.body.comment, function (err, dbnewComment) {
+                if (err) {
+                    console.log(err);
+                    res.redirect("/campgrounds");
+                } else {
+                    // Collego il nuovo commento al campground in db
+                    dbCamp.comments.push(dbnewComment._id);
+                    
+                    //Salvo
+                    dbCamp.save();
+
+                    // Infine ridirigo alla pagina corretta
+                    res.redirect("/campgrounds/" + dbCamp._id);
+                }
+            })
         }
     })
 });
